0547f86f23c71278b150576a8574ade323927d14
Pravin2311 <moonspayhq@gmail.com>
2025-12-14 17:13:57 +0530

    5de

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "aspirant-daily-v1";
+
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/quiz.html",
+  "/result.html",
+  "/styles.css",
+  "/js/quiz.js",
+  "/js/result.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
